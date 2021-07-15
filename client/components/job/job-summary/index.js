4c5ac47e6643df7ea19d82879a2e8af782795ff2
Prince-mountblue <prince.yadav.16@mountblue.tech>
2021-07-15 10:49:28 +0530

    Add the ui for jobs part

---
+++ client/components/job/job-summary/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link'
+import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict'
+import slug from 'slug'
+
+import Tag from '../../tag'
+
+import styles from './job-summary.module.css'
+import Button from '../../button'
+
+const JobSummary = ({
+  id,
+  name,
+  title,
+  location,
+  link,
+  description,
+  children
+}) => {
+  return (
+    <div className={styles.container}>
+      <Link href="/jobs/[slug]" as={`/jobs/${id}`}>
+        <a className={styles.link}>{title}</a>
+      </Link>
+      <div className={styles.excerpt}>{children}</div>
+      <div className={styles.container}>
+        <h1 className={styles.location}>Location:{location}</h1>
+        {/* <p>{salary}</p> */}
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.tagContainer}>
+          <Button href={link} primary target="_blank">
+            {name}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default JobSummary
